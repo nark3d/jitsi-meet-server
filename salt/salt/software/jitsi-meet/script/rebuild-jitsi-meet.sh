95a3e6f884f27260fe7491547a15e717e543ba4c
Adam Lewis <adam.lewis@surevine.com>
2018-01-15 16:38:52 +0000

    Removing unrequired uglify step.

--- salt/salt/software/jitsi-meet/script/rebuild-jitsi-meet.sh
+++ salt/salt/software/jitsi-meet/script/rebuild-jitsi-meet.sh
@@ -3,4 +3,4 @@
 # Simple script to ensure a current build of Jitsi Meet.
 
 cd /var/www/html/jitsi-meet
-npm install && make compile && make uglify && make deploy && make clean
+npm install && make compile && make deploy && make clean
